e4aa3d72a71923520e3bb04d62c2488d6f9f1401
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-18 21:58:02 -0700

    fixed betty styling

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ *main- Entry point
+ *
+ *Return: always 0
+ */
+
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
+puts("Programming is like building a multilingual puzzle\n");
 
-  return 0;
+return (0);
 }
