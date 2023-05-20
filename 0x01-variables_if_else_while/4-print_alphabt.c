17967ef0134c59ce55ae98cfacb63c1d2d49049c
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-19 19:48:32 -0700

    tasks 4-5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Start of program
+ *
+ * Return: Always (0);
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar("%c", letter);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
