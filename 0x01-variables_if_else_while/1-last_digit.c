9dc415e0b844311a47ea74c209e780f16e5d5088
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-19 18:48:40 -0700

    fixed undeclared var for real this time

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,9 @@ int main(void)
 {
 	int n, last;
 
-	last = n % 10;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	last = n % 10;
 
 	if (last > 5)
 	{
@@ -26,7 +25,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	else if (last < 6 && last > 0)
+	else if (last < 6 && last > 1)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
