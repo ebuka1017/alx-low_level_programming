a6b440a2e428e10ba4d7b56305d863dd9d5350b7
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-19 20:46:15 -0700

    fixed task 9 output error

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,11 @@
 
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			printf("%d %d, ", i, j);
-		}
+		printf("%d, ", i);
 	}
 
 	printf("\n");
