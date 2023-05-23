05c8b5052a7e5468a83aceff9c75b81cfe3c47ce
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-23 04:36:20 -0700

    replaced printf with _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,19 @@
-#ifndef MAIN_H 
-#define MAIN_H
+#ifndef _MAIN_H_ 
+#define _MAIN_H_
+
+/**
+ * _putchar - writes to standard output
+ *
+ * @c: character to compare
+ *
+ * Return: 1 or 0
+ */
+
+int _putchar(char c);
+
+/**
+ * print_alphabet - printsfunc to print alphabet
+ */
 void print_alphabet(void);
 
 #endif
