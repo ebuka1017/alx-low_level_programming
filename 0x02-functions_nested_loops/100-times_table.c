561493d5adb477e8af31cb5a1d133d8983d7e7e5
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-23 21:36:45 -0700

    extra credit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,41 +3,46 @@
 /**
  * print_times_table - prints the n times table
  *
+ *@n: count starts from here
+ *
  * Return: Always (0) success
  */
 
 void print_times_table(int n)
 {
-	int i, j, product;
-
-	for (i = 0; i <= n; i++)
+	while (n > 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		int i, j, product;
+
+		for (i = 0; i <= n; i++)
 		{
-			product = i * j;
-
-			if (j != 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				product = i * j;
+
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar('  ');
+				}
+
+				if (product < 10 && j != 0)
+					_putchar(' ');
+
+				if (product >= 10)
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+				else
+				{
+					_putchar(product + '0');
+				}
+
 			}
 
-			if (product < 10 && j != 0)
-				_putchar(' ');
-
-			if (product >= 10)
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
-			else
-			{
-				_putchar(product + '0');
-			}
-
+			_putchar('\n');
 		}
-
-		_putchar('\n');
 	}
 }
 
