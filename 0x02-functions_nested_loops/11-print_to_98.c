f50c4d19db77a1883726d721b8e0f0377c198e34
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-23 20:15:46 -0700

    task fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,44 @@
 #include "main.h"
+#include "6-abs.c"
+#include <stdio.h>
 
 /**
- * print_to_98 - counts up from a given value n to 98
+ * print_to_98 - prints from a given number n to 98
  *
- *@n: count starts here
+ * @n: count starts from here
  *
- * Return: Always (0)
+ * Return: always (0) success
  */
 
 void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n > 98)
 	{
-		_putchar(i);
-		_putchar(',');
-		_putchar(' ');
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
 	}
+
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
 }
 
