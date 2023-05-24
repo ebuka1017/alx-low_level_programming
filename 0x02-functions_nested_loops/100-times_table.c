16502f21b94028615074c8b398084f6d130b04ce
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-23 22:00:08 -0700

    task 12 fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,40 +10,41 @@
 
 void print_times_table(int n)
 {
-	while (n > 0 && n <= 15)
+	if (n < 0 || n > 15)
 	{
-		int i, j, product;
+		return;
+	}
+	int i, j, product;
 
-		for (i = 0; i <= n; i++)
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			product = i * j;
+
+			if (j != 0)
 			{
-				product = i * j;
-
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-
-				if (product < 10 && j != 0)
-					_putchar(' ');
-
-				if (product >= 10)
-				{
-					_putchar((product / 10) + '0');
-					_putchar((product % 10) + '0');
-				}
-				else
-				{
-					_putchar(product + '0');
-				}
-
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 
-			_putchar('\n');
+			if (product < 10 && j != 0)
+				_putchar(' ');
+
+			if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				_putchar(product + '0');
+			}
+
 		}
+
+		_putchar('\n');
 	}
 }
 
