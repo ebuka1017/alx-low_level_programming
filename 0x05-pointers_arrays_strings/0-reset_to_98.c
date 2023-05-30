c1e77d970bf548c5df48e187e2d020237106d758
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-29 18:11:32 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * return_to_98 - takes a pointer to an int as parameter and updates the value to (98)
+ * *n: pointer
+ *
+ * Return: always (0) success 
+ */
+
+void return_to_98(int *n)
+{
+	*n = 98;
+}
+
