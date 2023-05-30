9fd67c97eb9587a573e2b655655864c14f5c5540
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-29 18:39:57 -0700

    fixed typos

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * return_to_98 - updates to 98
+ * reset_to_98 - updates to 98
  * @n: pointer integer
  */
 
-void return_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
