2c7afdd53f51bd09b94a75b00f0ac0802eb99f3c
ebuka1017 <isaacokwuzi@gmail.com>
2023-05-29 20:34:39 -0700

    task 5 fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,12 +15,13 @@ void rev_string(char *s)
 
 	length = 0;
 	start = 0;
-	end = length - 1;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
+
+	end = length - 1;
 
 	while (start < end)
 	{
@@ -32,7 +33,6 @@ void rev_string(char *s)
 		end--;
 	}
 
-	_putchar('\n');
 }
 
 
