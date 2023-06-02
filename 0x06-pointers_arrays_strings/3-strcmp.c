a3279f9d75c0beb0ca65168e5a2f8f3e86a8052b
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-01 17:51:01 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: (0) is s1==s2' (-1) if s1 < s2, (1) if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else if (*s1 < *s2)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
