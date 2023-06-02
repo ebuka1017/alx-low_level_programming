7c31b9395547b1ec53e4434869dd44d467a6b236
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-01 18:14:41 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,17 +17,7 @@ int _strcmp(char *s1, char *s2)
 		s2++;
 	}
 
-	if (*s1 == *s2)
-	{
-		return (0);
-	}
-	else if (*s1 < *s2)
-	{
-		return (-1);
-	}
-	else
-	{
-		return (1);
-	}
+
+	return (*s1 - *s2);
 }
 
