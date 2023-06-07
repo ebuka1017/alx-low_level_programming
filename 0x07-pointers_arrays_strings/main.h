64627eba6179c67c29e9b586e409a2d7bc0c656f
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-06 17:28:55 -0700

    task 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -172,5 +172,11 @@ int _strcmp(char *s1, char *s2);
 
 char *_memset(char *s, char b, unsigned int n);
 
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 #endif
 
