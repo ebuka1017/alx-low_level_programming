61aae96d2265a1afd18473bcbc5775fd3a213c50
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-08 19:19:40 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: string
+ *
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
+
