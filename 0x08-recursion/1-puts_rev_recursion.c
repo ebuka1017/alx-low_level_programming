04615fd138975a94157c3988ae179e8b6cdeef48
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-08 19:42:59 -0700

    task 1

---
+++ 0x08-recursion/1-puts_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: string
+ *
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+
+}
+
