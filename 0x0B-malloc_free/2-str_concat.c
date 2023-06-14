353b0b15b48c693dc7b98e739cecdfcab9e16a84
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-13 19:47:49 -0700

    added missing ref

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,40 @@
 #include "main.h"
 #include <stdlib.h>
 
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ *
+ * @dest: destination string
+ * @src: string to be added
+ *
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *new_s = dest;
+
+	while (*new_s != '\0')
+	{
+		new_s++;
+	}
+
+	while (*src  != '\0')
+	{
+		*new_s = *src;
+
+		new_s++;
+		src++;
+	}
+
+	*new_s = '\0';
+
+	return (dest);
+}
+
+
 /**
  * str_concat - concatenates two strings
  * @s1: string 1
