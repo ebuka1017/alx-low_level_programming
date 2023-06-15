6ca6b840311b021662670499985b5ee8df14c60e
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-14 19:28:50 -0700

    task 2 fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -13,13 +13,12 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc > 0)
+
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
+		printf("%s\n", argv[i]);
 	}
+
 
 	return (0);
 }
