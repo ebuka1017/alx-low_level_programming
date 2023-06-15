b93be3f8e55ea68893f7b56f6b6b534fb485c8f7
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-14 19:41:58 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", mul);
+
+		return (0);
+	}
+
+	else
+		printf("Error\n");
+
+	return (1);
+}
+
