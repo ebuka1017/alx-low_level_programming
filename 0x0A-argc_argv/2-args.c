faae75425fb744095ecf14b70a3fc88173e72b20
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-14 19:25:29 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument vector
+ * @argv: argument count
+ *
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+
+	return (0);
+}
+
