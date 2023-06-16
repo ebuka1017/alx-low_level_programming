e97bd818e6978ff823917e9599f5cc4f2f51bd2d
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-15 18:33:54 -0700

    task 3

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -310,5 +310,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 void *_calloc(unsigned int nmemb, unsigned int size);
 
+/**
+ * array_range - creates an array of integers
+ */
+
+int *array_range(int min, int max);
+
 #endif
 
