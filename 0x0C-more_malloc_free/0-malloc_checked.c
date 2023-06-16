8faedfa440794363e8f91d80c78a8fc50ef1dea6
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-15 17:49:00 -0700

    fixed betty styling

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,13 @@
 
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
+	void *ptr = malloc(b);
 
-    if (ptr == NULL)
-    {
-        exit(98);
-    }
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
 
-    return ptr;
+	return (ptr);
 }
 
