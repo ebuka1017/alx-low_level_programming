6d8748f03b11e47c699a543259c885e048ef3ad5
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-19 20:08:12 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees the memory allocated to dogs
+ *
+ * @d: dog to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
+
