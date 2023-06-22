d7b4cce4f76447e0815a5a93260e88c64482b477
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-22 16:10:12 -0700

    task 0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+/**
+ * sum_them_all - returns the sum of all it's params
+ *
+ * @n: number oftimes to iterate
+ *
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
+
