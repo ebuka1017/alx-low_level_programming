0ca21f0af6e0b5a02405f94cbb43118663914879
ebuka1017 <isaacokwuzi@gmail.com>
2023-06-22 16:45:35 -0700

    corrected typo

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -31,7 +31,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		}
 
-		if (separator != null && i < n - 1)
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
 		}
