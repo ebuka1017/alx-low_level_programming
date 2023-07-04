6e74c6ba2232f41e4876162ecc82885fc2906803
ebuka1017 <isaacokwuzi@gmail.com>
2023-07-03 19:05:22 -0700

    task 0 fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,18 +1,47 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print all elements of list_t
+ * @h: pointer to the list head
+ *
+ * Return: number of nodes in list
+ */
 
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	const list_h *current = h;
 
-	while (current != NULL)
+	if (h == NULL)
 	{
-		printf("[%zu] %s\n", count, current->str);
-		count++;
-		current = current->next;
+	    printf("[0] (nil)\n");
+	    
+	    return (count);
 	}
+
+	printf("[");
+
+	while (h != NULL)
+	{
+	if (h->str != NULL)
+	{
+		printf("%s", h->str);
+	}
+	else
+	{
+		printf("[0] (nil)");
+	}
+
+	count++;
+
+	h = h->next;
+
+	if (h != NULL)
+	{
+		printf(", ");
+	}
+	}
+	printf("]\n");
 
 	return (count);
 }
-
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-size_t print_list(const list_t *h);
-
-
-typedef struct list_t{
+typedef struct list_t
+{
 	char *str;
 	struct list_t *next;
 } list_t;
+
+size_t print_list(const list_t *h);
 
 #endif
 
