1a09dd2cf46618fcc59da66b3ef4f954c6c4dd6b
ebuka1017 <isaacokwuzi@gmail.com>
2023-07-10 20:04:55 -0700

    advanced task fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -21,13 +21,20 @@ void display_error(const char *message)
  */
 void display_elf_header(const Elf64_Ehdr *header)
 {
+	int i;
+
 	printf("Magic: ");
-	for (int i = 0; i < EI_NIDENT; i++)
+
+	for (i = 0; i < EI_NIDENT; i++)
 		printf("%02x ", header->e_ident[i]);
 	printf("\n");
 
-	printf("Class: %s\n", (header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
-	printf("Data: %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
+	printf("Class: %s\n",
+	       (header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("Data: %s\n",
+	       (header->e_ident[EI_DATA] == ELFDATA2LSB)
+		       ? "2's complement, little endian"
+		       : "2's complement, big endian");
 	printf("Version: %d\n", header->e_ident[EI_VERSION]);
 	printf("OS/ABI: %d\n", header->e_ident[EI_OSABI]);
 	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
@@ -44,16 +51,20 @@ void display_elf_header(const Elf64_Ehdr *header)
  */
 int main(int argc, char *argv[])
 {
+	const char *filename;
+	int fd;
+	Elf64_Ehdr header;
+	ssize_t bytes_read;
+
 	if (argc != 2)
 		display_error("Usage: elf_header elf_filename");
 
-	const char *filename = argv[1];
-	int fd = open(filename, O_RDONLY);
+	filename = argv[1];
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		display_error("Error: Failed to open the file");
 
-	Elf64_Ehdr header;
-	ssize_t bytes_read = read(fd, &header, sizeof(header));
+	bytes_read = read(fd, &header, sizeof(header));
 	if (bytes_read == -1 || bytes_read != sizeof(header))
 	{
 		close(fd);
